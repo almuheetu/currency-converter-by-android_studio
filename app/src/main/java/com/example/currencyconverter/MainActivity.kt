ddd9cf5e883f4ac8c29ae947b0514bf759fa35c9
Almuheetu Shihab <almuheetu@gmail.com>
2024-06-03 14:50:57 +0600

    pack problem solved and pack init

--- app/src/main/java/com/example/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/example/currencyconverter/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.currencyconverter
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -12,6 +14,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,13 +24,12 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
         val showResultTv = findViewById<TextView>(R.id.tv_show_result)
         val ustToBdtButton = findViewById<Button>(R.id.btn_test)
         val bdtToUsdButton = findViewById<Button>(R.id.bt_test)
         val editText = findViewById<EditText>(R.id.et_test)
-        val euroToBdtButton = findViewById<Button>(R.id.btns_test)
-        val bdtToEuroButton = findViewById<Button>(R.id.btnss_test)
+        val euroToBdtButton = findViewById<Button>(R.id.second_btn)
+        val bdtToEuroButton = findViewById<Button>(R.id.second_btn_two)
 
         ustToBdtButton.setOnClickListener {
             if (editText.text.toString().isBlank() && !checkIsValidInput(editText.text.toString().toInt())) {
@@ -57,6 +59,8 @@ class MainActivity : AppCompatActivity() {
         }
 
         bdtToEuroButton.setOnClickListener {
+            Log.d("Log404","The value of")
+
             if (!checkIsValidInput(editText.text.toString().toInt())) {
                 Toast.makeText(applicationContext, "Convert is not possible", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
@@ -69,10 +73,13 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    fun checkIsValidInput(currencyValue: Int): Boolean {
+    private fun checkIsValidInput(currencyValue: Int): Boolean {
+        Log.d("Log404","The value of $currencyValue")
+
         return currencyValue > 10
     }
 
+    fun EditViewOneOnClick(view: View) {}
 
 
 //    fun EditViewOneOnClick(view: View) {
